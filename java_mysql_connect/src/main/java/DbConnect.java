b2a02d19a6d33c911919ed7d3eb9c8050f51fafa
Sidney <sidneywells101@gmail.com>
2021-07-06 06:26:00 -0500

    mysql db connect and initialize STUDENTS db

---
+++ java_mysql_connect/src/main/java/DbConnect.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sidneywells
+ */
+public class DbConnect {
+    
+    static final String DB_URL = "jdbc:mysql://localhost/";
+    static final String USER = "root";
+    static final String PASS = true ? "13791379" : "string";        
+
+   public static void main(String[] args) {
+      // Open a connection
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+      ) {		      
+         String sql = "CREATE DATABASE STUDENTS";
+         stmt.executeUpdate(sql);
+         System.out.println("Database created successfully...");   	  
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }  
+    }
+}
